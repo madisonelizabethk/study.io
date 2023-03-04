aec56f854944c5ffdb8119748379cbd9a1572ff2
alexia rodriguez <alexia.rodriguez11211@gmail.com>
2023-03-04 12:06:33 -0600

    test commit

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -1,64 +1,64 @@
 type ClassInfo = {
-    classID: number
-    className: string
-    classTimes: number
-    startDate: string
-    courseDescription: string
-    officeHours: string
-    professorEmail: string
-    classTextbook: string
-    gradingScale: number
-}
+  classID: number;
+  className: string;
+  classTimes: number;
+  startDate: string;
+  courseDescription: string;
+  officeHours: string;
+  professorEmail: string;
+  classTextbook: string;
+  gradingScale: number;
+};
 
 type StudentInfo = {
-    username: string
-    studentID: number
-    firstName: string
-    lastName: string
-    userEmail: string
-    classification: string
-}
+  username: string;
+  studentID: number;
+  firstName: string;
+  lastName: string;
+  userEmail: string;
+  classification: string;
+};
 
-type StudyTimer ={
-    startTime: number
-    shortBreakTime: number
-    longBreakTime: number
-    endTime: number
-    taskCompletion: Array<Task>
-}
+type StudyTimer = {
+  startTime: number;
+  shortBreakTime: number;
+  longBreakTime: number;
+  endTime: number;
+  taskCompletion: Array<Task>;
+};
 
-type Task ={
-    name: string
-    type: string
-    dueDate: string
-    dueTime: number
-}
+type Task = {
+  name: string;
+  type: string;
+  dueDate: string;
+  dueTime: number;
+};
 
-type Calendar ={
-    dueDate: string
-}
+type Calendar = {
+  dueDate: string;
+};
 
 type notification = {
-    taskInfo: Task
-    exam: ExamSchedule
-}
+  taskInfo: Task;
+  exam: ExamSchedule;
+};
 
 type GradeCalculator = {
-    currentGrade: number
-    gradeWeight: number
-    letterGrade: string
-}
+  currentGrade: number;
+  gradeWeight: number;
+  letterGrade: string;
+};
 
 type StudySession = {
-    vocabTerm: string
-    vocabDefinition: string 
-    rightAnswerCounter: number
-    wrongAnswerCounter: number
-}
+  vocabTerm: string;
+  vocabDefinition: string;
+  rightAnswerCounter: number;
+  wrongAnswerCounter: number;
+};
 
 type ExamSchedule = {
-    pointsPossible: number
-    letterGrade: string
-    examTime: number
-    examDate: string
-}
+  pointsPossible: number;
+  letterGrade: string;
+  examTime: number;
+  examDate: string;
+};
