6caecbb906e1e7a8fb92f2cd53902b9941049ba9
Madison Kennedy <madisonelizabethk@gmail.com>
2023-03-08 09:09:36 -0600

    update 3/8/23

---
+++ src/entities/ClassInfo.ts
@@ -0,0 +1,31 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
+export class ClassInfo {
+  @PrimaryGeneratedColumn('uuid')
+  classID: string;
+
+  @Column({ unique: true })
+  className: string;
+
+  @Column({ unique: true })
+  studentID: string;
+
+  @Column({ unique: true })
+  passwordHash: string;
+
+  @Column({ unique: true })
+  classTimes: number;
+
+  @Column({ default: 0 })
+  classTextbook: string;
+
+  @Column({ default: 0 })
+  gradingScale: number;
+
+  @Column({ unique: true })
+  courseDescription: string;
+
+  @Column({ default: false })
+  professorEmail: string;
+}
