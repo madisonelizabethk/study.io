d076de706be5b2df3d00a24dee7e0d81c72c70e9
Madison Kennedy <madisonelizabethk@gmail.com>
2023-03-15 18:17:21 -0500

    updated user entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,19 +1,34 @@
-import { Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   userID: string;
 
-  @Column({ unique: true})
+  @PrimaryGeneratedColumn('uuid')
+  studentID: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column({ unique: true })
+  firstName: string;
+
+  @Column({ default: 0 })
+  lastName: string;
+
+  @Column({ unique: true })
   email: string;
 
-  @Column({ unique: true})
+  @Column({ unique: true })
+  classification: string;
+
+  @Column({ unique: true })
   passwordHash: string;
 
-  @Column ({ default: false})
+  @Column({ default: false })
   verifiedEmail: boolean;
 
-  @Column({default: 0})
+  @Column({ default: 0 })
   profileViews: number;
 }
