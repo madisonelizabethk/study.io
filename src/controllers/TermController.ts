5a411b79585c28eb2dfd9c88c77e007215dfc6d2
Madison Kennedy <madisonelizabethk@gmail.com>
2023-04-05 10:38:30 -0500

    added html files and term controller

---
+++ src/controllers/TermController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { allTermData, getTermsFromUser } from '../models/TermModel';
+
+async function getTerm(req: Request, res: Response): Promise<void> {
+  const { termID } = req.params as { termID: string };
+
+  const term = await getTermsFromUser(termId);
+
+  if (!term) {
+    res.sendStatus(404); // Not found
+    return;
+  }
+
+  res.status(200).json(term);
+}
+
+async function getAllTerms(req: Request, res: Response): Promise<void> {
+  res.status(200).json(await getTermsFromUser());
+}
+
+export { getTerm, getAllTerms };
